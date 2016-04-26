0590c0043a3a68c192715a9775cce5dce0e11165
patrick brisbin <pbrisbin@gmail.com>
2016-04-26 18:27:46 -0400

    Implement

--- src/Golf.hs
+++ src/Golf.hs
@@ -4,11 +4,36 @@ module Golf
     , histogram
     ) where
 
+import Data.List (maximumBy, transpose)
+import Data.Ord (comparing)
+
 skips :: (Eq a, Show a) => [a] -> [[a]]
-skips = undefined
+skips = go 1
+  where
+    go n xs = case every n xs of
+        [] -> []
+        xs' -> xs' : go (n + 1) xs
+
+    every :: Int -> [a] -> [a]
+    every n = concat . zipWith (\i x -> [x|i `mod` n == 0]) [1..]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima (x:(rest@(y:z:_)))
+    | y > x && y > z = y : localMaxima rest
+    | otherwise = localMaxima rest
+localMaxima _ = []
 
-histogram :: [Integer] -> String
-histogram = undefined
+histogram :: [Int] -> String
+histogram xs = unlines $ transpose $ map column hs
+  where
+    column :: (Int, Int) -> String
+    column (n, h) = concat
+        [ replicate (maxH - h) ' '
+        , replicate h '*', "=", show n
+        ]
+
+    hs :: [(Int, Int)]
+    hs = map (\n -> (n, length $ filter (== n) xs)) [0..9]
+
+    maxH :: Int
+    maxH = snd $ maximumBy (comparing snd) hs
